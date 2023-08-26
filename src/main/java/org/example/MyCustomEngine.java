cbc51e63852b2ceac06598880a71ac7f895e337f
kurs <kwp@tlen.pl>
2023-08-26 10:18:23 +0200

    10:18

---
+++ src/main/java/org/example/MyCustomEngine.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.ExecutionRequest;
+import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.TestEngine;
+import org.junit.platform.engine.UniqueId;
+import org.junit.platform.engine.support.descriptor.EngineDescriptor;
+
+Public class MyCustomEngine implements TestEngine {
+
+    public static final String ENGINE_ID = "my-custom-engine";
+
+    @Override
+    public String getId() {
+        return ENGINE_ID;
+    }
+
+    @Override
+    public TestDescriptor discover(EngineDiscoveryRequest discoveryRequest,
+                                   UniqueId uniqueId) {
+        // Discover test(s) and return a TestDescriptor object
+        TestDescriptor testDescriptor = new EngineDescriptor(uniqueId,
+                "My test");
+        return testDescriptor;
+    }
+
+    @Override
+    public void execute(ExecutionRequest request) {
+        // Use ExecutionRequest to execute TestDescriptor
+        TestDescriptor rootTestDescriptor = request.getRootTestDescriptor();
+        request.getEngineExecutionListener()
+                .executionStarted(rootTestDescriptor);
+    }
+
+}
