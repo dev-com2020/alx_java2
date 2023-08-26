7dbb0c171b69bf502600f8003fb7f0cb8955f425
kurs <kwp@tlen.pl>
2023-08-26 12:30:06 +0200

    12:30

--- src/test/java/org/example/StandardAssertionsTest.java
+++ src/test/java/org/example/StandardAssertionsTest.java
@@ -1,4 +1,15 @@
 package org.example;
 
-public class StandardAssertionsTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StandardAssertionsTest {
+
+    @Test
+    void standardAssertions(){
+        assertEquals(2,2);
+        assertTrue(true,"Opcjonalna wiadomość");
+        assertFalse(false,()-> "Bardzo" + " dziwna" + " struktura");
+    }
 }
