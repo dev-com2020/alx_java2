17cc96a6b39ca7b00d41871abb707c0869a60598
kurs <kwp@tlen.pl>
2023-08-26 15:42:56 +0200

    15:42

---
+++ src/main/java/org/example/Os.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public enum Os {
+    WINDOWS,MAC,LINUX,OTHER
+
+    public static Os determine() {
+        Os out = OTHER;
+        String myOs = System.getProperty("os.name").toLowerCase();
+        if (myOs.contains("win")){
+            out = WINDOWS;
+        }
+    }
+}
--- src/test/java/org/example/SimpleTaggingTest.java
+++ src/test/java/org/example/SimpleTaggingTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 //import org.testng.annotations.Test;
 
 
-@Tag("non-functional")
+@Tag("simple")
 class SimpleTaggingTest {
 
     @Test
