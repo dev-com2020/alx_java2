5180e991496228e40ffc48c6fec4ee1def950a44
kurs <kwp@tlen.pl>
2023-08-27 10:01:03 +0200

    27.08 10:00

--- src/test/java/org/example/DynamicExampleTest.java
+++ src/test/java/org/example/DynamicExampleTest.java
@@ -1,5 +1,19 @@
 package org.example;
 
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+import java.util.stream.Stream;
+
 public class DynamicExampleTest {
 
+    @TestFactory
+    Stream<DynamicTest> dynamicTestFromStream(){
+        Stream<String> inputStream = Stream.of("A","B","C");
+        return inputStream.map(
+                input -> DynamicTest.dynamicTest("Wyświetlana nazwa dla inputu " + input,
+                        ()-> {
+                            System.out.println("Testowane " + input);
+                        }));
+    }
 }
