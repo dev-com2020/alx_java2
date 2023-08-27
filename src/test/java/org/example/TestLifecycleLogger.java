e6c4257a575c8ca5a490106e1978c9f309585f09
kurs <kwp@tlen.pl>
2023-08-27 11:07:51 +0200

    27.08 11:07

--- src/test/java/org/example/TestLifecycleLogger.java
+++ src/test/java/org/example/TestLifecycleLogger.java
@@ -1,4 +1,29 @@
 package org.example;
 
-public class TestLifecycleLogger {
+import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface TestLifecycleLogger {
+    static final Logger log = LoggerFactory.getLogger(TestLifecycleLogger.class.getName());
+
+    @BeforeAll
+    static void beforeAllTests(){
+        log.info("beforeAllTests");
+    }
+
+    @AfterAll
+    static void afterAllTests(){
+        log.info("afterAllTests");
+    }
+
+    @BeforeEach
+    default void beforeEachTest(TestInfo testInfo){
+        log.info("Informacje o wykonaniu {}", testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    default void afterEachTest(TestInfo testInfo){
+        log.info("Zakończenie wykonania {}", testInfo.getDisplayName());
+    }
 }
